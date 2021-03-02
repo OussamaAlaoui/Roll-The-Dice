975dff3899bb3a820be4f5bfbbced9fdc73c50e9
OussamaAlaoui <ousalis99@gmail.com>
2021-03-02 17:14:55 +0100

    add: playing status
    playing status give us the ability to know, if the game is active or not to activate or disactivate roll and  hold buttons from functionning till the game is in active mode.

--- roleDiceGame.js
+++ roleDiceGame.js
@@ -13,6 +13,7 @@ let total = document.querySelector('#' + totalScoreId);
 let dice = 0;
 let active = 0;
 let winnerName = '';
+let playingStat = true;
 let winner = document.querySelector('.player-name');
 const whoStarts = function () {
   active = Math.floor(Math.random() * 2) + 1;
@@ -28,45 +29,50 @@ const passRoll = function () {
 };
 
 start.addEventListener('click', function () {
-  whoStarts();
-  roll.classList.remove('hidden');
-  hold.classList.remove('hidden');
-  start.classList.add('hidden');
-  player = document.querySelector('.player' + active);
-  player.classList.add('active');
-  let currentScore = 0;
-  let totalScore = 0;
-  roll.addEventListener('click', function () {
-    // genarting a random dice roll between 1-6
-    dice = Math.floor(Math.random() * 6) + 1;
-    //display dice image
-    diceImage.classList.remove('hidden');
-    diceImage.src = `images/dice-${dice}.png`;
-    total = document.querySelector('#' + totalScoreId + active);
-    current = document.querySelector('#' + currentScoreId + active);
-    currentScore += dice;
-    current.textContent = currentScore;
-    if (dice === 1) {
+  if (playingStat) {
+    whoStarts();
+    roll.classList.remove('hidden');
+    hold.classList.remove('hidden');
+    start.classList.add('hidden');
+    player = document.querySelector('.player' + active);
+    player.classList.add('active');
+    let currentScore = 0;
+    let totalScore = 0;
+    roll.addEventListener('click', function () {
+      // genarting a random dice roll between 1-6
+      dice = Math.floor(Math.random() * 6) + 1;
+      //display dice image
+      diceImage.classList.remove('hidden');
+      diceImage.src = `images/dice-${dice}.png`;
+      total = document.querySelector('#' + totalScoreId + active);
+      current = document.querySelector('#' + currentScoreId + active);
+      currentScore += dice;
+      current.textContent = currentScore;
+      if (dice === 1) {
+        passRoll();
+      }
+    });
+    hold.addEventListener('click', function () {
+      totalScore = Number(total.textContent);
+      totalScore += currentScore;
+      total.textContent = totalScore;
+      if (total.textContent >= 10) {
+        player.classList.add('winner');
+        winner = document.querySelector('.player-name' + active);
+        console.log(`${winnerName} IS THE WINNER 🎉`);
+        roll.classList.add('hidden');
+        hold.classList.add('hidden');
+        playingStat = false;
+      }
       passRoll();
-    }
-  });
-  hold.addEventListener('click', function () {
-    totalScore = Number(total.textContent);
-    totalScore += currentScore;
-    total.textContent = totalScore;
-    if (total.textContent >= 10) {
-      player.classList.add('winner');
-      winner = document.querySelector('.player-name' + active);
-      console.log(`${winnerName} IS THE WINNER 🎉`);
-      roll.classList.add('hidden');
-      hold.classList.add('hidden');
-    }
-    passRoll();
-    currentScore = 0;
-    current.textContent = 0;
-  });
+      currentScore = 0;
+      current.textContent = 0;
+    });
+  }
 });
+
 restart.addEventListener('click', function () {
+  playingSta = true;
   document.querySelector('#total-score-p1').textContent = 0;
   document.querySelector('#total-score-p2').textContent = 0;
   for (let i = 0; i < allPlayers.length; i++) {
